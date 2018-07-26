d596d73006b9edf24e645c2d9d7a875548bb600c
Matthew Lalmansingh <lalmansingh.matthew@heb.com>
2018-07-26 13:18:22 -0500

    Add Swagger Docs to Basic Endpoint
    - Add Swagger decorators to auto generate Swagger docs

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask
+from flask_restplus import Resource, Api
 
 # Asks app to import Flask.  Flask is used to create instances of Web Applications
 # __name__ is a special var in Python.  __main__ can be used if it's being executed
 # as main program
+
 app = Flask(__name__)
+api = Api(app, version='1.0', title='Sample API',
+    description='A Sample API')
 
 # Decorator defines route
-@app.route("/hello")
-def hello():
-    return "Hello World"
+@api.route('/hello')
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
 
 if __name__ == '__main__':
     app.run(debug=True)
